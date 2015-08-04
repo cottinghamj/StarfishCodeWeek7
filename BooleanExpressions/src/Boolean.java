d1e76a27a8e2bb25866eb82931d554880ad392c2
cottinghamj <cottinghamja@gmail.com>
2015-08-04 09:46:40 -0500

    Review Day 1 added

---
+++ BooleanExpressions/src/Boolean.java
@@ -0,0 +1,37 @@
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		// Boolean expressions are statements that evaluate to either true of false
+		
+		// All the types of boolean
+		/*
+		 * >	 - greater than
+		 * <	 - less than
+		 * >=	 - greater than or equal 
+		 * <=	 - less than or equal
+		 * ==	 - equal to 
+		 * !=	 - not equal to
+		 * 
+		 * Combinatorial Logic
+		 * && 	- All must be true 
+		 * 		eg. if(age > 50 && age < 100 && year > 2015)
+		 * 
+		 * ||	- Or -> One of them must be true
+		 * 		eg. if(age > 50 || age < 25 || year > 2015)
+		 * 
+		 * 
+		 * 20 > 15 -> True
+		 * 15 < 2 -> False
+		 * 9/3 > 3 -> False
+		 * 9 * 3 + 1 == 28 -> True
+		 * 8 == 8 && 5 == 5 -> True
+		 * 7 - 8 > 0 -> False
+		 * 9 * 9 == 81 && 7 < 3 -> False
+		 * 
+		 * 
+		 * 
+		 */
+	}
+
+}
